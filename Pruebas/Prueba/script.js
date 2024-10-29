b29e8769f189e63b7e52d02d511a6965edee4a6d
AloncraftMC <alonso.ensibemol@gmail.com>
2024-10-29 23:36:15 +0100

    29/10/2024

---
+++ Pruebas/Prueba/script.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("element");
+const h1 = document.getElementById("h1");
+
+document.addEventListener("wheel", event => {
+    h1.textContent = "HICISTE ALGO"
+});
